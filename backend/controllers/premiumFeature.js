f15331e449b6722a3315026dad60f411b4a0b64c
Mohammad Ayub Ansari <119043948+MohammadAyubAnsari@users.noreply.github.com>
2023-08-05 19:56:56 +0530

    10.Leaderboard Optimisation

--- backend/controllers/premiumFeature.js
+++ backend/controllers/premiumFeature.js
@@ -4,35 +4,23 @@ const User = require("../models/users");
 
 const getUserLeaderBoard = async (req, res) => {
   try {
-    const users = await User.findAll();
-    console.log(users);
-    const expenses = await Expense.findAll();
-    console.log(expenses);
-    const userAggregatedExpenses = {};
-    expenses.forEach((expense) => {
-      if (userAggregatedExpenses[expense.userId]) {
-        userAggregatedExpenses[expense.userId] =
-          userAggregatedExpenses[expense.userId] + expense.expenseamount;
-      } else {
-        userAggregatedExpenses[expense.userId] = expense.expenseamount;
-      }
+    const LeaderboardofUsers = await User.findAll({
+      attributes: [
+        "id",
+        "name",
+        [sequelize.fn("sum", sequelize.col("expenseamount")), "total_cost"],
+      ],
+      include: [
+        {
+          model: Expense,
+          attributes: [],
+        },
+      ],
+      group: ["user.id"],
+      order: [[sequelize.col("total_cost"), "DESC"]],
     });
 
-    const userLeaderBoard = [];
-    users.forEach((user) => {
-      userLeaderBoard.push({
-        name: user.name,
-
-        total_cost: userAggregatedExpenses[user.id] || 0,
-      });
-    });
-    // userLeaderBoard.sort((a, b) => b.amount - a.amount);
-    userLeaderBoard.sort((a, b) => b.total_cost - a.total_cost);
-
-    console.log(userLeaderBoard);
-
-    console.log(userAggregatedExpenses);
-    res.status(200).json(userLeaderBoard);
+    res.status(200).json(LeaderboardofUsers);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
